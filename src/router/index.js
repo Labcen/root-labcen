2ad6f730a84d1a2cc283c4e6509d02286005b34d
Labcen <122981540@qq.com>
2022-12-02 00:43:15 +0800

    feat: 测试

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/login",
+    name: "Login",
+
+    component: () => import("../views/login"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
